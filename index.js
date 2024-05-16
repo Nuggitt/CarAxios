1e5ec04164993e58fdc77247a739812ed6c4c1db
Nuggitt <phulip.nielsen@gmail.com>
2024-05-17 00:28:02 +0200

    it works flawlessly

--- index.js
+++ index.js
@@ -3,25 +3,35 @@ const baseurl = "https://carrestapi20240417213626.azurewebsites.net/api/cars";
 Vue.createApp({
     data() {
         return {
-            cars:[],
-            car : null,
-
+            cars: [],
+            singleCar: null,
+            addCarData: { id: 0, brand: "", model: "", horsePower: 0 },
+            addMessage: "",
 
         }
     },
     methods: {
-        async helperGetAndShow(baseurl) {
+        async getAllCars() {
+            await this.helperGetAndShow(baseurl);
+        },
+
+        async helperGetAndShow(url) {
             try {
-                const response = await axios.get(baseurl);
+                const response = await axios.get(url);
                 this.cars = response.data;
             } catch (ex) {
                 alert(ex.message);
             }
         },
-        async getAllCars() {
-            await this.helperGetAndShow(baseurl);
-        }
-
-
-    },
-}).mount('#app')
+        
+        async addCar() {
+            try {
+              response = await axios.post(baseurl, this.addCarData);
+              this.addMessage = "response " + response.status + " " + response.statusText;
+              this.getAllCars();
+            } catch (ex) {
+              alert(ex.message);
+            }
+          },
+    }
+}).mount('#app');
